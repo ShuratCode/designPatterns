3fac9f5e5df3e4a6c39968e18e4e2c1536a735e7
Shaked Eyal <shaked.eyal@protonmail.com>
2022-11-12 23:24:25 +0200

    Using Singleton on the ChocolateBoiler

--- src/main/java/org/sigleton/ChocolateBoiler.java
+++ src/main/java/org/sigleton/ChocolateBoiler.java
@@ -1,12 +1,21 @@
 package org.sigleton;
 
-public class BoilerFactory {
+public class ChocolateBoiler {
     private boolean empty;
     private boolean boiled;
 
-    public BoilerFactory() {
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler() {
         empty = true;
         boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
     }
 
     public void fill() {
